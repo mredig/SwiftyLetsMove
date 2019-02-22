2a1d72a180d06e4c4ea8650c0ae45ffdc515ae86
Michael Redig <mredig@gmail.com>
2019-02-21 20:26:16 -0600

    Initial Commit

---
+++ SwiftyLetsMove/SwiftyLetsMove.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLetsMove.h
+//  SwiftyLetsMove
+//
+//  Created by Michael Redig on 2/21/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyLetsMove.
+FOUNDATION_EXPORT double SwiftyLetsMoveVersionNumber;
+
+//! Project version string for SwiftyLetsMove.
+FOUNDATION_EXPORT const unsigned char SwiftyLetsMoveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLetsMove/PublicHeader.h>
+
+
